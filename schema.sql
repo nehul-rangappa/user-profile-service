4801fc0fa5b3170dc7e59e11a9519ebe00f0e5f8
Nehul Rangappa <nrangapp@iu.edu>
2024-06-14 02:13:49 -0400

    Added foreign key constraint to users table

--- schema.sql
+++ schema.sql
@@ -1,16 +1,17 @@
 -- DDL Script for Gigawrks Database --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `users`(
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `country` varchar(50) NOT NULL,
+  `country_id` int NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-);
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  CONSTRAINT `country_fk` FOREIGN KEY (`id`) REFERENCES `countries` (`id`)
+)
 
 CREATE TABLE IF NOT EXISTS `countries`(
   `id` int NOT NULL AUTO_INCREMENT,
