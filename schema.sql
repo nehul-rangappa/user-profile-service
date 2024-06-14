9d73dbad5561f7754ae8b5e618ad3db361b4c247
Nehul Rangappa <nrangapp@iu.edu>
2024-06-14 02:13:10 -0400

    Added DDL for countries table

--- schema.sql
+++ schema.sql
@@ -10,4 +10,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-); 
+);
+
+CREATE TABLE IF NOT EXISTS `countries`(
+  `id` int NOT NULL AUTO_INCREMENT,
+  `common_name` varchar(50) NOT NULL,
+  `official_name` varchar(100) DEFAULT NULL,
+  `country_code` varchar(30) NOT NULL,
+  `capital` varchar(50) DEFAULT NULL,
+  `region` varchar(50) DEFAULT NULL,
+  `sub_region` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `country_code_UNIQUE` (`country_code`)
+)
